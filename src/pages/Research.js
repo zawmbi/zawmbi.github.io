0e07b43d47d418ed65f9b701aeaf79791688505f
Linda Mansour <124932177+zawmbi@users.noreply.github.com>
2025-01-09 10:08:29 -0600

    testing again

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -6,7 +6,6 @@ function Research() {
     return (
 <div>
     <h1> Thank you for your patience while this page is under construction! </h1>
-    <h1> Thank you for your patience while this page is under construction! </h1>
 
 </div>
     );
